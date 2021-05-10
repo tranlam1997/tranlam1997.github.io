2739c25d840ecb91e11d8f9bad5b501c66757736
Tran Tuan Lam <53012411+lamtuantran1997@users.noreply.github.com>
2021-05-10 13:20:57 +0700

    Add files via upload

--- javascript/animate_main.js
+++ javascript/animate_main.js
@@ -2,8 +2,7 @@ const test_1 = document.querySelector("#test-level div:first-child div:first-chi
 const test_2 = document.querySelector("#test-level div:first-child div:nth-child(2)");
 
 function test_show_up(){
-    console.log(window.innerHeight)
-    if(window.innerHeight > 1350){
+    if(window.pageYOffset > 1350){
         test_1.style.animation = "rise-up-1 3s linear 1 forwards";
         test_2.style.animation = "rise-up-2 3s linear 1 forwards";
     }
