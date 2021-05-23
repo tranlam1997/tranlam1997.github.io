0d6dc415e0d503cb5048e3ac20339ef67799641e
Tran Tuan Lam <53012411+lamtuantran1997@users.noreply.github.com>
2021-05-24 03:28:18 +0700

    Add files via upload

--- javascript/animate_main.js
+++ javascript/animate_main.js
@@ -1,6 +1,6 @@
 const test_1 = document.querySelector("#test-level div:first-child div:first-child");
 const test_2 = document.querySelector("#test-level div:first-child div:nth-child(2)");
-const mediaQuery = window.matchMedia("(max-width:499px)");
+const mediaQuery = window.matchMedia("(max-width: 500px)");
 const nav_mobile_icon = document.querySelector("#nav-mobile-icon");
 
 
@@ -8,13 +8,14 @@ function adjust_device(mediaQuery){
     if(mediaQuery.matches){
         offset.style.display = "none";
         nav_mobile_icon.style.display = "block";
-        if(window.pageYOffset > 200){
+        if(window.pageYOffset > 100){
             test_1.style.animation = "rise-up-1 3s linear 1 forwards";
             test_2.style.animation = "rise-up-2 3s linear 1 forwards";
         }
     } else{
-    if(window.pageYOffset > 1350){
         nav_mobile_icon.style.display = "none";
+    if(window.pageYOffset > 1350){
+        
         test_1.style.animation = "rise-up-1 3s linear 1 forwards";
         test_2.style.animation = "rise-up-2 3s linear 1 forwards";
     }
